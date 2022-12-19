79a0c870c1e0a157d197012b458a452914e04d5c
Nassim LOUNADI <nassimlnd37@gmail.com>
2022-12-19 14:32:10 +0100

    ✅Added profil info, and confirmationDialog for deleteCommande (Admin)

--- src/main/java/com/mmn/circuitscourts/controller/client/ProfilController.java
+++ src/main/java/com/mmn/circuitscourts/controller/client/ProfilController.java
@@ -1,8 +1,14 @@
 package com.mmn.circuitscourts.controller.client;
 
 import com.mmn.circuitscourts.App;
+import com.mmn.circuitscourts.models.Client;
+import com.mmn.circuitscourts.services.ClientDAO;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.input.MouseEvent;
+
+import java.sql.SQLException;
 
 public class ProfilController {
 
@@ -11,8 +17,31 @@ public class ProfilController {
     @FXML
     Label labelGrade;
 
-    public void initialize() {
+    @FXML
+    Label identifiant;
+    @FXML
+    Label password;
+    @FXML
+    Label numTel;
+    @FXML
+    Label clientName;
+
+    public void initialize() throws SQLException {
         labelUsername.setText(App.userConnected.getIdentifiant());
         labelGrade.setText(App.userConnected.getGradeName());
+
+        identifiant.setText(App.userConnected.getIdentifiant());
+        password.setText(App.userConnected.getPassword());
+        numTel.setText(getClient().getNumTel());
+        clientName.setText(getClient().getNom());
+    }
+
+    public Client getClient() throws SQLException {
+        ClientDAO clientDAO = new ClientDAO();
+        return clientDAO.getByAccountId(App.userConnected.getId());
+    }
+
+    public void onBrowse() {
+
     }
 }
