55e5c78f9a9114745031dd01b31d57635f75664d
emsamax <martin.neron@laposte.net>
2022-12-26 18:32:15 +0100

    start javaDOC on admin.account  and admin.AddAccount

--- src/main/java/com/mmn/circuitscourts/controller/admin/AccountController.java
+++ src/main/java/com/mmn/circuitscourts/controller/admin/AccountController.java
@@ -18,137 +18,177 @@ import java.util.ArrayList;
 
 public class AccountController {
 
-        @FXML
-        VBox contentTable;
-        @FXML
-        Button addButton;
-        @FXML
-        VBox successPopup;
-        @FXML
-        VBox confirmationDialog;
-        @FXML
-        Button okButton;
-        @FXML
-        Button cancelButton;
-        @FXML
-        Label descDialog;
-        static VBox popup;
+    @FXML
+    VBox contentTable;
+    @FXML
+    Button addButton;
+    @FXML
+    VBox successPopup;
+    @FXML
+    VBox confirmationDialog;
+    @FXML
+    Button okButton;
+    @FXML
+    Button cancelButton;
+    @FXML
+    Label descDialog;
+    static VBox popup;
+
+    /**
+     * Méthode qui est appelé lors du chargement de la page fxml associée a ce controller.
+     * Récupère via la DAO accountDAO tous les comptes constituants la BD
+     * Pour chaque comptes on crée une ligne avec la fonction createLine() contenant les informations de ce compte.
+     * initialisation de la popup de la confirmation.
+     *
+     * @throws SQLException
+     */
+    public void initialize() throws SQLException {
+        ArrayList<User> users = User.accountDAO.getAll();
+        users.forEach(user -> {
+            try {
+                createLine(user);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        popup = successPopup;
+    }
+
+    /**
+     * méthode appelé lors de la suppression d'un compte, rend visible la popup de confirmation de la supression
+     */
+    public static void showSuccessPopUp() {
+        popup.setVisible(true);
+    }
 
 
-        public void initialize() throws SQLException {
-            ArrayList<User> users = User.accountDAO.getAll();
-            users.forEach(user -> {
-                try {
-                    createLine(user);
-                } catch (SQLException e) {
-                    throw new RuntimeException(e);
-                }
-            });
-            popup = successPopup;
-        }
+    /**
+     * La page fxml est constituée d'un tableau, ce tableau n'est pas un tableau fxml il est seulement composée de lignes et de labels.
+     * Permet la créarion d'une ligne du tableau pour pour un seul compte et de remplire  la ligne avec les informations de ce compte.
+     * A la fin de chaque ligne crée les bouttons de modification et de suppression de la ligne.
+     *
+     * @param user le compte d'un utilisateur
+     * @throws SQLException
+     */
+    public void createLine(User user) throws SQLException {
+        HBox line = new HBox();
+        line.setAlignment(Pos.CENTER_LEFT);
+        line.setPrefHeight(64);
+        line.setPrefWidth(850);
+        line.setPadding(new Insets(0, 0, 0, 40));
+        line.getStyleClass().add("commande-tableview-line");
+        ArrayList<Label> labels = new ArrayList<>();
+        Label id = new Label(String.valueOf(user.getId()));
+        Label identifiant = new Label(user.getIdentifiant());
+        Label mdp = new Label(user.getGradeName());
+        Label grade = new Label(String.valueOf(user.getPassword()));
+        labels.add(id);
+        labels.add(identifiant);
+        labels.add(grade);
+        labels.add(mdp);
 
-        public static void showSuccessPopUp() {
-            popup.setVisible(true);
-        }
+        labels.forEach(label -> {
+            label.getStyleClass().add("commande-tableview-line-cell");
+            label.setMaxHeight(1.7976931348623157E308);
+            label.setPrefHeight(1.7976931348623157E308);
+            label.setPrefWidth(141);
+            label.setMinWidth(141);
+            label.setMaxWidth(141);
+            line.getChildren().add(label);
+        });
 
+        Button edit = new Button();
+        edit.getStyleClass().add("edit-button");
+        Region editImg = new Region();
+        editImg.getStyleClass().add("edit-button-img");
+        edit.setGraphic(editImg);
+        edit.setPickOnBounds(true);
+        HBox.setMargin(edit, new Insets(0, 0, 0, 30));
+        edit.setOnMouseClicked(mouseEvent -> {
+            onEdit(user.getId());
+        });
 
-        public void createLine(User user) throws SQLException {
-            HBox line = new HBox();
-            line.setAlignment(Pos.CENTER_LEFT);
-            line.setPrefHeight(64);
-            line.setPrefWidth(850);
-            line.setPadding(new Insets(0, 0, 0, 40));
-            line.getStyleClass().add("commande-tableview-line");
-            ArrayList<Label> labels = new ArrayList<>();
-            Label id = new Label(String.valueOf(user.getId()));
-            Label identifiant = new Label(user.getIdentifiant());
-            Label mdp = new Label(user.getGradeName());
-            Label grade = new Label(String.valueOf(user.getPassword()));
-            labels.add(id);
-            labels.add(identifiant);
-            labels.add(grade);
-            labels.add(mdp);
+        Button delete = new Button();
+        delete.getStyleClass().add("delete-button");
+        Region deleteImg = new Region();
+        deleteImg.getStyleClass().add("delete-button-img");
+        delete.setGraphic(deleteImg);
+        delete.setPickOnBounds(true);
+        delete.setOnMouseClicked(event -> {
+            showConfirmationDialog(user.getId());
+        });
 
-            labels.forEach(label -> {
-                label.getStyleClass().add("commande-tableview-line-cell");
-                label.setMaxHeight(1.7976931348623157E308);
-                label.setPrefHeight(1.7976931348623157E308);
-                label.setPrefWidth(141);
-                label.setMinWidth(141);
-                label.setMaxWidth(141);
-                line.getChildren().add(label);
-            });
+        line.getChildren().add(edit);
+        line.getChildren().add(delete);
+        contentTable.getChildren().add(line);
+    }
 
-            Button edit = new Button();
-            edit.getStyleClass().add("edit-button");
-            Region editImg = new Region();
-            editImg.getStyleClass().add("edit-button-img");
-            edit.setGraphic(editImg);
-            edit.setPickOnBounds(true);
-            HBox.setMargin(edit, new Insets(0, 0, 0, 30));
-            edit.setOnMouseClicked(mouseEvent -> {
-                onEdit(user.getId());
-            });
-
-            Button delete = new Button();
-            delete.getStyleClass().add("delete-button");
-            Region deleteImg = new Region();
-            deleteImg.getStyleClass().add("delete-button-img");
-            delete.setGraphic(deleteImg);
-            delete.setPickOnBounds(true);
-            delete.setOnMouseClicked(event -> {
-                showConfirmationDialog(user.getId());
-            });
-
-            line.getChildren().add(edit);
-            line.getChildren().add(delete);
-            contentTable.getChildren().add(line);
-        }
-
-        public void onAddButton() {
-            ViewFactory.getInstance().showAdminAddAccountInterface();
-        }
+    /**
+     * Méthode appelée lorsqu'un utilisateur appuie sur le boutton ajoutter sur la page fxml.
+     * Appel de la méthode showAdminAddAccountInterface() de la classe ViewFactory qui permet de changer de scene dans l'application.
+     */
+    public void onAddButton() {
+        ViewFactory.getInstance().showAdminAddAccountInterface();
+    }
 
 
-        public void onClosePopup() {
-            successPopup.setVisible(false);
-        }
+    /**
+     * Fermeture de la popup de confirmation.
+     */
+    public void onClosePopup() {
+        successPopup.setVisible(false);
+    }
+
+    /**
+     * Méthode appelée lorsqu'un utilisateur appuie sur le boutton modifier sur la page fxml.
+     * Appel de la méthode showAdminEditAccountInterface() de la classe ViewFactory qui permet de changer de scene dans l'application.
+     * @param AccountId qui est l'id du compte à modifier.
+     */
+    private void onEdit(int AccountId) {
+        ViewFactory.getInstance().showAdminEditAccountInterface(AccountId);
+    }
 
 
-        private void onEdit(int AccountId) {
-            ViewFactory.getInstance().showAdminEditAccountInterface(AccountId);
-        }
+    /**
+     * Suppression du compte et de la ligne de ce compte constituant le tableau.
+     * La suppression se fait via l'appel de la méthode remove de la classe accountDAO
+     * @param accountId id du compte à supprimer
+     * @throws SQLException
+     */
+    private void onDelete(int accountId) throws SQLException {
+        User.accountDAO.remove(accountId);
+        System.out.println("[DEBUG]Account deleted");
+        contentTable.getChildren().clear();
+        ArrayList<User> users = User.accountDAO.getAll();
+        users.forEach(user -> {
+            try {
+                createLine(user);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
 
-
-        private void onDelete(int accountId) throws SQLException {
-            User.accountDAO.remove(accountId);
-            System.out.println("[DEBUG]Account deleted");
-            contentTable.getChildren().clear();
-            ArrayList<User> users = User.accountDAO.getAll();
-            users.forEach(user -> {
-                try {
-                    createLine(user);
-                } catch (SQLException e) {
-                    throw new RuntimeException(e);
-                }
-            });
-        }
-
-        public void showConfirmationDialog(int accountId) {
-            descDialog.setText("Voulez vous vraiment supprimer le compte n°"+ accountId);
-            confirmationDialog.setVisible(true);
-            okButton.setOnMouseClicked(mouseEvent -> {
-                confirmationDialog.setVisible(false);
-                try {
-                    onDelete(accountId);
-                } catch (SQLException e) {
-                    throw new RuntimeException(e);
-                }
-            });
-            cancelButton.setOnMouseClicked(mouseEvent -> {
-                confirmationDialog.setVisible(false);
-            });
-        }
+    /**
+     * Affichage de la confiramtion de suppression du compte pour plus de sécurité.
+     * La suppressin de compte ne s'effectue seulement si l'utilisateur clique sur le boutton "confirmer"
+     * @param accountId
+     */
+    public void showConfirmationDialog(int accountId) {
+        descDialog.setText("Voulez vous vraiment supprimer le compte n°" + accountId);
+        confirmationDialog.setVisible(true);
+        okButton.setOnMouseClicked(mouseEvent -> {
+            confirmationDialog.setVisible(false);
+            try {
+                onDelete(accountId);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        cancelButton.setOnMouseClicked(mouseEvent -> {
+            confirmationDialog.setVisible(false);
+        });
+    }
 }
 
 
--- src/main/java/com/mmn/circuitscourts/controller/admin/AddAccountController.java
+++ src/main/java/com/mmn/circuitscourts/controller/admin/AddAccountController.java
@@ -19,14 +19,25 @@ public class AddAccountController {
     @FXML
     PasswordField mdp, confirmMdp;
 
+    /**
+     * Est déclenchée lors du chargement de la page fxml associée à ce controller.
+     */
     public void initialize(){
         getGradesInitialize();
     }
 
+    /**
+     * Effectue le changement d page lors d'un clique sur le boutton retour, renvoie à la page Account.fxml
+     */
     public void onBackButton() {
         ViewFactory.getInstance().showAdminAccountInterface();
     }
 
+    /**
+     * récupereation des valeurs de tous les champs modifiables présents dans la page fxml et crée un utilisateur avec ces valeurs.
+     * si le compte est crée renvoi à la page Account.fxml.
+     * @throws SQLException
+     */
     public void onCreateButton() throws SQLException {
         if(Objects.equals(mdp.getText(), confirmMdp.getText())){
             User user = new User(identifiant.getText(), String.valueOf(confirmMdp.getText()), getGradeNumber(grade.getValue()));
@@ -38,6 +49,9 @@ public class AddAccountController {
 
     }
 
+    /**
+     * initialisation des tous les grades existants dans la comboBox contenant les grades, est appelée dans la méthode initialize.()
+     */
     public void getGradesInitialize(){
         ArrayList<String> gradesNames = new ArrayList<>();
         gradesNames.add("Client");gradesNames.add("Producteur");gradesNames.add("Administrateur");
@@ -45,6 +59,11 @@ public class AddAccountController {
         grade.getItems().addAll(gradesNames);
     }
 
+    /**
+     * méthode associant un numéro en fonction du nom du grade, les grades correspondent à des numéros dans la base de données.
+     * @param grade
+     * @return
+     */
     public int getGradeNumber(String grade){
         switch (grade){
             case "Client" :
